793d45f090abdfd9d1282af8cd85f87271740e64
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-08 13:47:37 +0100

    Esercizio svolto per metà, tentativo di fix errore riga 57

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const  membri = [
+    {
+        'nome':'Wayne Barnett',
+        'ruolo':'Founder & CEO',
+        'immagineProfilo': 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome': 'Angela Caroll',
+        'ruolo': 'Chief Editor',
+        'immagineProfilo': 'img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nome':'Walter Gordon',
+        'ruolo': 'Office Manager',
+        'immagineProfilo': 'img/walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome': 'Angela Lopez',
+        'ruolo':'Social Media',
+        'immagineProfilo':'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome':'Scott Estrada',
+        'ruolo':'Developer',
+        'immagineProfilo':'img/scott-estrada-developer.jpg'
+    },
+    {
+        'nome':'Barbara Ramos',
+        'ruolo': 'Graphic Designer',
+        'immagineProfilo': 'img/barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+const main = document.getElementById('main');
+
+for (i = 0; i < membri.length; i++) {
+    const listaMembri = createNewCard(membri);
+    main.append(listaMembri);
+}
+
+
+
+
+
+
+
+
+
+function createNewCard (membri) {
+   const card = document.createElement('div');
+   card.classList.add('card');
+   const cardBorder = document.createElement('div');
+   cardBorder.classList.add('card-border-top');
+   card.append(cardBorder);
+   const cardImg = document.createElement('img');
+   cardImg.classList.add('img');
+   cardImg.src = (membri[i+1]['immagineProfilo']);
+   card.append(cardImg);
+   const cardPerson = document.createElement('span');
+   cardPerson.innerHTML = (membri[i+1]['nome'])
+   card.append(cardPerson);
+   const cardTitle = document.createElement('p');
+   cardTitle.classList.add('job');
+   cardTitle.innerHTML = membri[i+1]['ruolo'];
+   card.append(cardTitle);
+
+   return card;
+   return cardBorder;
+   return cardImg;
+   return cardPerson;
+   return cardTitle;
+}
